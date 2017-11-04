ca9e38eab94babd89de2464807151638df9c626e
Ugi <ugi@waifu.club>
2017-11-04 21:44:04 +0100

    got rid of unnecessary code

--- src/osu.c
+++ src/osu.c
@@ -13,9 +13,6 @@
 
 GLuint init_program(const char* vs_path, const char* gs_path, const char* fs_path);
 
-GLuint init_approach_vao(GLuint* vbo_pos, GLuint* vbo_uv);
-GLuint init_circle_vao(GLuint* vbo_pos, GLuint* vbo_uv);
-
 void init_textures(GLuint* tex, GLenum texnum, const char* path);
 
 int main(int argc, char* argv[]) {
@@ -57,14 +54,6 @@ int main(int argc, char* argv[]) {
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-	//glFrontFace(GL_CCW);
-
-	//GLuint approach_pos, approach_uv;
-	//GLuint approach_vao = init_approach_vao(&approach_pos, &approach_uv);
-	//GLuint approach_program = init_program("resources/glsl/approach.vert", "resources/glsl/approach.frag");
-
-	//GLuint circle_pos, circle_uv;
-	//GLuint circle_vao = init_circle_vao(&circle_pos, &circle_uv);
 	GLuint circle_program = init_program("resources/glsl/circle.vert", "resources/glsl/circle.geom", "resources/glsl/circle.frag");
 	GLuint approach_program = init_program("resources/glsl/approach.vert", "resources/glsl/approach.geom", "resources/glsl/approach.frag");
 
@@ -147,105 +136,6 @@ int main(int argc, char* argv[]) {
 		}
 
 		glDisableVertexAttribArray(0);
-
-		//glBindVertexArray(circle_vao);
-		//glEnableVertexAttribArray(0);
-		//glEnableVertexAttribArray(1);
-
-		//// hit circle drawing
-		//{
-		//	glUseProgram(circle_program);
-
-		//	glUniform1i(circle_texID, 0);
-		//	glUniformMatrix4fv(circle_mvpID, 1, GL_FALSE, mvp);
-		//	glUniform1f(circle_timeID, (float)glfwGetTime());
-
-		//	glDrawArrays(GL_TRIANGLES, 0, CIRCLE_BUFFER_SIZE * 6);
-		//}
-
-		//// approach circle drawing
-		//{
-		//	glUseProgram(approach_program);
-		//
-		//	glUniform1i(approach_texID, 1);
-		//	glUniformMatrix4fv(approach_mvpID, 1, GL_FALSE, mvp);
-		//	glUniform1f(approach_timeID, (float)glfwGetTime());
-
-		//	glDrawArrays(GL_TRIANGLES, 0, CIRCLE_BUFFER_SIZE * 6);
-		//}
-
-		//glDisableVertexAttribArray(0);
-		//glDisableVertexAttribArray(1);
-
-		//glUseProgram(approach_program);
-		//glBindVertexArray(approach_vao);
-		//glEnableVertexAttribArray(0);
-		//glEnableVertexAttribArray(1);
-
-		//glUniform1i(texID, 0);
-
-		//float t = (float)glfwGetTime();
-		//if (circle_buffer[circle_off + circle_span].t - ar < t)
-		//	circle_span++;
-		//else if (circle_buffer[circle_off].t + 0.2 < t) {
-		//	circle_span--;
-		//	circle_off++;
-		//}
-
-		//// circle drawing
-		//for (int i = circle_off + circle_span - 1; i >= circle_off; i--) {
-		//	float circle_model[16];
-		//	float mvp[16];
-
-		//	mat4_identity(circle_model);
-		//	mat4_identity(mvp);
-
-		//	float translation[3] = { 
-		//		circle_buffer[i].x,
-		//		circle_buffer[i].y,
-		//		circle_buffer[i].t
-		//	};
-		//	mat4_translate(circle_model, translation, circle_model);
-
-		//	mat4_identity(mvp);
-		//	mat4_multiply(projection, view, mvp);
-		//	mat4_multiply(mvp, circle_model, mvp);
-
-		//	glUniform1i(texID, 0);
-		//	glUniformMatrix4fv(mvpID, 1, GL_FALSE, mvp);
-
-		//	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-
-		//	if (circle_buffer[i].t > t)
-		//	{
-		//		float approach_model[16];
-		//		mat4_identity(approach_model);
-
-		//		translation[0] = 0.0f;
-		//		translation[1] = 0.0f;
-		//		translation[2] = -.1f;
-		//		mat4_translate(circle_model, translation, circle_model);
-
-		//		float scale[3] = {
-		//			1.0f + 2.0f * (circle_buffer[i].t - t) / ar,
-		//			1.0f + 2.0f * (circle_buffer[i].t - t) / ar,
-		//			1.0f + 2.0f * (circle_buffer[i].t - t) / ar
-		//		};
-		//		mat4_scale(circle_model, scale, approach_model);
-
-		//		mat4_identity(mvp);
-		//		mat4_multiply(projection, view, mvp);
-		//		mat4_multiply(mvp, approach_model, mvp);
-
-		//		glUniform1i(texID, 1);
-		//		glUniformMatrix4fv(mvpID, 1, GL_FALSE, mvp);
-
-		//		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
-		//	}
-		//}
-
-		//glDisableVertexAttribArray(1);
-		//glDisableVertexAttribArray(0);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -351,145 +241,6 @@ GLuint init_program(const char* vs_path, const char* gs_path, const char* fs_pat
 	free(fs_code);
 
 	return pID;
-}
-
-GLuint init_approach_vao(GLuint* vbo_pos, GLuint* vbo_uv) {
-	GLuint vao;
-
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
-
-	float vertex_data[8] = {
-		// point down left
-		-50.0f, -50.0f,
-		// point up left
-		-50.0f, +50.0f,
-		// point down right
-		+50.0f, -50.0f,
-		// point up right
-		+50.0f, +50.0f
-	};
-
-	glGenBuffers(1, vbo_pos);
-	glBindBuffer(GL_ARRAY_BUFFER, *vbo_pos);
-	glBufferData(
-		GL_ARRAY_BUFFER,
-		sizeof(vertex_data),
-		vertex_data,
-		GL_STATIC_DRAW
-	);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
-
-	float uv_data[8] = {
-		// point down left
-		0.0f, 1.0f,
-		// point up left
-		0.0f, 0.0f,
-		// point down right
-		1.0f, 1.0f,
-		// point up right
-		1.0f, 0.0f
-	};
-
-	glGenBuffers(1, vbo_uv);
-	glBindBuffer(GL_ARRAY_BUFFER, *vbo_uv);
-	glBufferData(
-		GL_ARRAY_BUFFER,
-		sizeof(uv_data),
-		uv_data,
-		GL_STATIC_DRAW
-	);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
-
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
-
-	return vao;
-}
-
-GLuint init_circle_vao(GLuint* vbo_pos, GLuint* vbo_uv) {
-	GLuint vao;
-
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
-
-	float vertex_data[CIRCLE_BUFFER_SIZE * 6 * 3];
-	float uv_data[CIRCLE_BUFFER_SIZE * 6 * 2];
-
-	for (size_t i = 0; i < CIRCLE_BUFFER_SIZE; i++) {
-		// counter clockwise
-
-		// first triangle
-
-		// point left down
-		vertex_data[i * 6 * 3 + 0] = circle_buffer[i].x - 50.0f;
-		vertex_data[i * 6 * 3 + 1] = circle_buffer[i].y - 50.0f;
-		vertex_data[i * 6 * 3 + 2] = circle_buffer[i].t;
-		uv_data[i * 6 * 2 + 0] = 0.0f;
-		uv_data[i * 6 * 2 + 1] = 0.0f;
-
-		// point right down
-		vertex_data[i * 6 * 3 + 3] = circle_buffer[i].x + 50.0f;
-		vertex_data[i * 6 * 3 + 4] = circle_buffer[i].y - 50.0f;
-		vertex_data[i * 6 * 3 + 5] = circle_buffer[i].t;
-		uv_data[i * 6 * 2 + 2] = 1.0f;
-		uv_data[i * 6 * 2 + 3] = 0.0f;
-
-		// point left up
-		vertex_data[i * 6 * 3 + 6] = circle_buffer[i].x - 50.0f;
-		vertex_data[i * 6 * 3 + 7] = circle_buffer[i].y + 50.0f;
-		vertex_data[i * 6 * 3 + 8] = circle_buffer[i].t;
-		uv_data[i * 6 * 2 + 4] = 0.0f;
-		uv_data[i * 6 * 2 + 5] = 1.0f;
-
-		// second triangle
-
-		// point right down
-		vertex_data[i * 6 * 3 + 9] = circle_buffer[i].x + 50.0f;
-		vertex_data[i * 6 * 3 + 10] = circle_buffer[i].y - 50.0f;
-		vertex_data[i * 6 * 3 + 11] = circle_buffer[i].t;
-		uv_data[i * 6 * 2 + 6] = 1.0f;
-		uv_data[i * 6 * 2 + 7] = 0.0f;
-
-		// point right up
-		vertex_data[i * 6 * 3 + 12] = circle_buffer[i].x + 50.0f;
-		vertex_data[i * 6 * 3 + 13] = circle_buffer[i].y + 50.0f;
-		vertex_data[i * 6 * 3 + 14] = circle_buffer[i].t;
-		uv_data[i * 6 * 2 + 8] = 1.0f;
-		uv_data[i * 6 * 2 + 9] = 1.0f;
-
-		// point left up
-		vertex_data[i * 6 * 3 + 15] = circle_buffer[i].x - 50.0f;
-		vertex_data[i * 6 * 3 + 16] = circle_buffer[i].y + 50.0f;
-		vertex_data[i * 6 * 3 + 17] = circle_buffer[i].t;
-		uv_data[i * 6 * 2 + 10] = 0.0f;
-		uv_data[i * 6 * 2 + 11] = 1.0f;
-	}
-
-	glGenBuffers(1, vbo_pos);
-	glBindBuffer(GL_ARRAY_BUFFER, *vbo_pos);
-	glBufferData(
-		GL_ARRAY_BUFFER,
-		sizeof(vertex_data),
-		vertex_data,
-		GL_STATIC_DRAW
-	);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-
-	glGenBuffers(1, vbo_uv);
-	glBindBuffer(GL_ARRAY_BUFFER, *vbo_uv);
-	glBufferData(
-		GL_ARRAY_BUFFER,
-		sizeof(uv_data),
-		uv_data,
-		GL_STATIC_DRAW
-	);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
-
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
-
-	return vao;
 }
 
 void init_textures(GLuint* tex, GLenum texnum, const char* path) {
